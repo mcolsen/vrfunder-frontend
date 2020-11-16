75cbd52b0722e2b0abb4a2faffeeaf783b40e75e
Maria Olsen <69918976+mcolsen@users.noreply.github.com>
2020-11-16 13:38:04 -0700

    Login and register API operations

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,54 @@
 import Container from "react-bootstrap/Container";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { setUsername, setToken, setLoginStatus } from "../state/userSlice";
 
 function Register() {
-	return <Container>Register</Container>;
+	const dispatch = useDispatch();
+	const history = useHistory();
+	const [registerError, setRegisterError] = useState(false);
+
+	const postNewUser = (username, password, role) => {
+		//	Role MUST be an integer, 1 for fundraiser and 2 for fundraiser
+		axios
+			.post("https://lamba-test.herokuapp.com/api/auth/register", {
+				username: username,
+				password: password,
+				role: role,
+			})
+			.then((res) => {
+				console.log(res);
+				axios
+					.post("https://lamba-test.herokuapp.com/api/auth/login", {
+						username: username,
+						password: password,
+					})
+					.then((res) => {
+						console.log(res);
+						dispatch(setToken(res.data.token));
+						dispatch(setUsername(username));
+						dispatch(setLoginStatus(true));
+						history.push("/");
+					})
+					.catch((err) => {
+						console.error(err);
+						setRegisterError(true);
+					});
+			})
+			.catch((err) => {
+				console.error(err);
+				setRegisterError(true);
+			});
+	};
+
+	return (
+		<Container>
+			Register
+			{registerError ? "Error: Please try again later." : ""}
+		</Container>
+	);
 }
 
 export default Register;
